9997c98015a0c3119c48d3470c24be6a0aaf41da
PaulinaAlfaro <36749091+PaulinaAlfaro@users.noreply.github.com>
2019-03-29 21:35:27 -0600

    Problema 04 - Conversión de tipos de datos
    Problema 04 - Conversión de tipos de datos

--- LeerCasos.py
+++ LeerCasos.py
@@ -4,23 +4,41 @@ def leerCasosPrueba(filename):
 
     for line in file:
         linelist = line.split(':')
+
         caso = {
             'no': linelist[0],
-            'funcion': linelist[1],
+            'metodo': linelist[1],
             'entrada': linelist[2],
             'salida': linelist[3],
         }
         casos.append(caso)
-
     file.close()
+    return casos
+
+
+def conversionTipos(casos):
+    for caso in casos:
+        entradasList = caso['entrada'].split(' ')
+        entradasConverted = []
+
+        for num in entradasList:
+            if num == "NULL":
+                entradasConverted.append(None)
+            else:
+                entradasConverted.append(float(num))
+
+        caso['entrada'] = entradasConverted
+
+        if caso['salida'] != 'Exception\n':
+            caso['salida'] = float(caso['salida'])
+
     return casos
 
 
 def main():
     casos = leerCasosPrueba("CasosPrueba.txt")
-    for caso in casos:
-        if caso['no'] == '0005':
-            print(caso)
+    casosConverted = conversionTipos(casos)
+    print(casosConverted)
 
 
 if __name__ == "__main__":
