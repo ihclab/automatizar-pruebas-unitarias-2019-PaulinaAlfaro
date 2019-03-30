d37019df202b4d762c39e0291abac3186b13db5f
PaulinaAlfaro <36749091+PaulinaAlfaro@users.noreply.github.com>
2019-03-29 21:37:03 -0600

    Problema 05 - Determinar el resultado del caso de prueba
    Problema 05 - Determinar el resultado del caso de prueba

--- LeerCasos.py
+++ LeerCasos.py
@@ -1,21 +1,22 @@
+from Medias import Medias
+import pprint
+
+
 def leerCasosPrueba(filename):
     file = open(filename, "r")
     casos = []
-
     for line in file:
         linelist = line.split(':')
-
         caso = {
             'no': linelist[0],
             'metodo': linelist[1],
             'entrada': linelist[2],
-            'salida': linelist[3],
+            'esperado': linelist[3],
         }
         casos.append(caso)
+
     file.close()
     return casos
-
-
 def conversionTipos(casos):
     for caso in casos:
         entradasList = caso['entrada'].split(' ')
@@ -23,14 +24,39 @@ def conversionTipos(casos):
 
         for num in entradasList:
             if num == "NULL":
-                entradasConverted.append(None)
+                entradasConverted.append(0)
             else:
                 entradasConverted.append(float(num))
 
         caso['entrada'] = entradasConverted
 
-        if caso['salida'] != 'Exception\n':
-            caso['salida'] = float(caso['salida'])
+        if caso['esperado'] != 'Exception\n':
+            caso['esperado'] = float(caso['esperado'])
+
+    return casos
+
+
+def probarCasos(casos):
+    medias = Medias()
+    for caso in casos:
+        try:
+            metodo = getattr(Medias, caso['metodo'])
+            result = metodo(medias, caso['entrada'])
+            esperado = caso['esperado']
+
+            caso['result'] = result
+
+            if esperado == result:
+                caso['valido'] = 'Exito'
+            else:
+                caso['valido'] = 'Falla'
+
+            print(metodo)
+            print(round(result, 4))
+            print(esperado)
+
+        except:
+            print('Exception')
 
     return casos
 
@@ -38,7 +64,8 @@ def conversionTipos(casos):
 def main():
     casos = leerCasosPrueba("CasosPrueba.txt")
     casosConverted = conversionTipos(casos)
-    print(casosConverted)
+    resultados = probarCasos(casosConverted)
+    pprint.pprint(resultados)
 
 
 if __name__ == "__main__":
