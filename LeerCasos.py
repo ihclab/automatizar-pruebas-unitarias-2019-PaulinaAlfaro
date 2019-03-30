2a1271df3a0b97e9a58e4abe29d4ccd514c3fc45
PaulinaAlfaro <36749091+PaulinaAlfaro@users.noreply.github.com>
2019-03-29 21:45:16 -0600

    Actividad 03 - Guardar los resultados en un archivo de texto
    Actividad 03 - Guardar los resultados en un archivo de texto

--- LeerCasos.py
+++ LeerCasos.py
@@ -17,6 +17,8 @@ def leerCasosPrueba(filename):
         casos.append(caso)
     file.close()
     return casos
+
+
 def conversionTipos(casos):
     for caso in casos:
         entradasList = caso['entrada'].split(' ')
@@ -30,11 +32,13 @@ def conversionTipos(casos):
         if caso['esperado'] != 'Exception\n':
             caso['esperado'] = float(caso['esperado'])
     return casos
+
+
 def probarCasos(casos):
     medias = Medias()
     for caso in casos:
         try:
-            #validacion del metodo ingresado
+            # validacion del metodo ingresado
             metodo = getattr(Medias, caso['metodo'])
             result = metodo(medias, caso['entrada'])
             esperado = caso['esperado']
@@ -43,17 +47,19 @@ def probarCasos(casos):
                 caso['valido'] = 'Exito'
             else:
                 caso['valido'] = 'Falla'
+
         except:
             print('Exception')
-            caso['result'] = 'Exception' 
+            caso['result'] = 'Exception'
             caso['valido'] = 'Falla'
 
     return casos
 
+
 def pintarResultados(resultados, time):
     print('\u001b[44;1m')
     print(" " * 50)
-    print("   PRUEBAS UNITARIAS AUTOMATIZADAS por Fernanda   ")
+    print("   PRUEBAS UNITARIAS AUTOMATIZADAS por Fernanda y Ale  ")
     print(" " * 50)
     print("   No de casos: {0:10d}".format(len(resultados)) + " " * 24)
     print("   Tiempo de ejecucion: {0:3.9f} sec".format(time) + " " * 11)
@@ -67,6 +73,24 @@ def pintarResultados(resultados, time):
         pprint.pprint(resultado)
 
 
+def generarResultados(resultados, time):
+    with open('resultados.txt', 'w') as file:
+        file.write("PRUEBAS UNITARIAS AUTOMATIZADAS por Fernanda y Ale\n")
+        file.write("casos: " + str(len(resultados)) + "\n")
+        file.write("tiempo: " + str(time) + '\n')
+        file.write("---------------------------------------------------\n")
+
+        for resultado in resultados:
+            file.write("No caso: " + str(resultado['no']) + '\n')
+            file.write("metodo: " + str(resultado['metodo']) + '\n')
+            file.write("entrada: " + str(resultado['entrada']) + '\n')
+            file.write("esperado: " + str(resultado['esperado']) + '\n')
+            file.write("resultado: " + str(resultado['result']) + '\n')
+            file.write("valido: " + str(resultado['valido']) + '\n')
+            file.write("- " * 26)
+            file.write('\n')
+
+
 def main():
     start = time.time()
     casos = leerCasosPrueba("CasosPrueba.txt")
@@ -74,6 +98,7 @@ def main():
     resultados = probarCasos(casosConverted)
     executetime = time.time()-start
     pintarResultados(resultados, executetime)
+    generarResultados(resultados, executetime)
 
 
 if __name__ == "__main__":
